e726195734bde9d167b87bd4093989e563ce9f4e
Ania Nakayama and Ben Dyer <ania.jurago+dyerb14@gmail.com>
2019-06-06 11:20:43 +0900

    Change database name

--- config/database.js
+++ config/database.js
@@ -77,7 +77,7 @@ module.exports = {
       port: Env.get("DB_PORT", "5432"),
       user: Env.get("DB_USER", ""),
       password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "adonis")
+      database: Env.get("DB_DATABASE", "proud_stories")
     }
   }
 };
