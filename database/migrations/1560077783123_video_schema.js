88fc89a6fa5b49a0c1d2edc8622f664ca6d3adf8
Konstantin Schlegel <konst2011@hotmail.de>
2019-06-11 09:00:28 +0200

    Added uploading video with rng name to aws & database, update video schema

--- database/migrations/1560077783123_video_schema.js
+++ database/migrations/1560077783123_video_schema.js
@@ -10,7 +10,7 @@ class VideoSchema extends Schema {
       table
         .integer("user_id")
         .notNullable()
-        .unique();
+        .references(users.id);
       table.string("url", 150).notNullable().unique();
       table.string("title", 150).notNullable();
       table.string("description", 300);
@@ -19,7 +19,7 @@ class VideoSchema extends Schema {
     });
   }
 
-  down () {
+  down() {
     this.drop('videos')
   }
 }
--- start/routes.js
+++ start/routes.js
@@ -17,46 +17,58 @@
 const Route = use("Route");
 const Video = use("App/Models/Video");
 const Drive = use('Drive');
+const randomstring = require("randomstring");
 
-Route.get("/videos", async ({ response }) => {
+Route.get("/videos", async ({
+  response
+}) => {
   const videos = await Video.all();
   response.send(videos);
 });
 
-Route.get("videos/:id", async ({ params }) => {
+Route.get("videos/:id", async ({
+  params
+}) => {
   const video = await Video.find(params.id);
   return video;
 });
 
-Route.get("users/:id", async ({ params }) => {
+Route.get("users/:id", async ({
+  params
+}) => {
   const user = await user.find(params.id);
   return user;
 });
 
-Route.get("users/:id/videos", async ({ params }) => {
+Route.get("users/:id/videos", async ({
+  params
+}) => {
   const user = await user.find(params.id);
   const videos = await Video.where("user_id", user.id);
   return videos;
 });
 
-Route.post('upload', async ({ request, response }) => {
+Route.post('upload', async ({
+  request,
+  response
+}) => {
   const body = request.post()
+
+  const video = new Video();
+  request.multipart.field((name, value) => {
+    video[name] = value;
+  })
+
+  request.multipart.file('video', {}, async (file) => {
+    const newFile = randomstring.generate() + ".mp4";
+    await Drive.disk('s3').put(newFile, file.stream);
+    video.url = Drive.disk('s3').getUrl(newFile);
+  })
+  await request.multipart.process()
 
   const Database = use('Database')
   const trx = await Database.beginTransaction()
 
-  request.multipart.file('video', {}, async (file) => {
-    await Drive.disk('s3').put(file.clientName, file.stream)
-  })
-
-  await request.multipart.process()
-
-  const video = new Video();
-  video.user_id = body.user_id;
-  video.title = body.title;
-  video.description = body.description;
-  video.url = body.url;
-  
   await video.save(trx)
-  trx.commit()
+  trx.commit();
 })
