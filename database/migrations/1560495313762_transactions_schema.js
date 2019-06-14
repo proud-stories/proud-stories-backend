af9d7bb1c1566cb618a6767511fc4534a5f57d16
Ben Dyer <dyer.ben@protonmail.com>
2019-06-14 16:47:30 +0900

    Add type to transactions database

--- database/migrations/1560495313762_transactions_schema.js
+++ database/migrations/1560495313762_transactions_schema.js
@@ -8,10 +8,11 @@ class TransactionsSchema extends Schema {
     this.create('transactions', (table) => {
       table.increments()
       table.timestamps()
-      table.integer('user_id_sender').unsigned().notNullable();
-      table.integer('user_id_receiver').unsigned().notNullable();
+      table.integer('sender_id').unsigned().notNullable();
+      table.integer('receiver_id').unsigned().notNullable();
       table.integer('amount').unsigned().notNullable();
       table.string('token_id');
+      table.string('type');
     })
   }
 
