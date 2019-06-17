ce6eeaafc3fd8a5b863566fd67b73647f12f69d9
Ben Dyer <dyerb14@gmail.com>
2019-06-17 16:13:44 +0900

    Change categories schema fields to snake case

--- database/migrations/1560659716251_video_categories_schema.js
+++ database/migrations/1560659716251_video_categories_schema.js
@@ -1,22 +1,34 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class VideoCategoriesSchema extends Schema {
   up() {
-    this.create('video_categories', (table) => {
-      table.integer('videoid').unsigned().notNullable();
-      table.integer('catid').unsigned().notNullable();
-      table.foreign('videoid').references('videos.id').onDelete();
-      table.foreign('catid').references('categories.id').onDelete();
+    this.create("video_categories", (table) => {
+      table
+        .integer("video_id")
+        .unsigned()
+        .notNullable();
+      table
+        .integer("cat_id")
+        .unsigned()
+        .notNullable();
+      table
+        .foreign("video_id")
+        .references("videos.id")
+        .onDelete();
+      table
+        .foreign("cat_id")
+        .references("categories.id")
+        .onDelete();
       table.timestamps();
-    })
+    });
   }
 
   down() {
-    this.drop('video_categories')
+    this.drop("video_categories");
   }
 }
 
-module.exports = VideoCategoriesSchema
+module.exports = VideoCategoriesSchema;
