0e6cd506ec28796e295d11a6420cb410ba88162a
Ben Dyer <dyer.ben@protonmail.com>
2019-06-19 08:04:40 +0900

    Remove axios, using built in Adonis API client

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -1,17 +1,9 @@
 'use strict'
-const axios = require('axios');
-const randomstring = require('randomstring')
-
-//Note axios needs the http:// prefix. Fetch API is not compatible(?) says undefined.
-
 const { test, trait } = use('Test/Suite')('User')
+const randomstring = require('randomstring')
 const User = use('App/Models/User')
 
 trait('Test/ApiClient')
-
-// test('make sure 2 + 2 is 4', async ({ assert }) => {
-//   assert.equal(2 + 2, 4)
-// })
 
 test('Should GET users at /users', async ({ client, assert }) => {
   //insert a fake user using Adonis
