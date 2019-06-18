fc58aa5bfc49d12d145d8439e299f61fe8fc3439
Ben Dyer <dyer.ben@protonmail.com>
2019-06-19 07:48:20 +0900

    Write test for GET users endpoint

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -1,7 +1,34 @@
 'use strict'
+const axios = require('axios');
+const randomstring = require('randomstring')
 
-const { test } = use('Test/Suite')('User')
+//Note axios needs the http:// prefix. Fetch API is not compatible(?) says undefined.
 
-test('make sure 2 + 2 is 4', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
+const { test, trait } = use('Test/Suite')('User')
+const User = use('App/Models/User')
+
+trait('Test/ApiClient')
+
+// test('make sure 2 + 2 is 4', async ({ assert }) => {
+//   assert.equal(2 + 2, 4)
+// })
+
+test('Should GET an array of users at /users', async ({ client, assert }) => {
+  const fakeName = randomstring.generate(30);
+  await User.create({
+    name: fakeName
+  })
+  
+  const response = await client.get('/users').end()
+  response.assertStatus(200);
+  response.assertJSONSubset([{name: fakeName}])
+  
+  // client.delete('/users').send({name: fakeName});
+  // await axios.get('http://localhost:3333/users').then(res => {
+  //   const users = res.data;
+  //   // console.log(users)
+  //   assert.
+  //   assert.equal(users.length > 0, true);
+  //   // assert.equal(users.length > 0, true)
+  // })
 })
