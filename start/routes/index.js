4f507d1e0193ab828b23ab7401fb3c569ea53e44
Ben Dyer <dyer.ben@protonmail.com>
2019-06-21 01:24:28 +0900

    Add query for users videos with aggregates at /users/:id/videos

--- start/routes/index.js
+++ start/routes/index.js
@@ -83,6 +83,73 @@ Route.get("videos/:id", async ({ params }) => {
   const video = await Video.find(params.id);
   return video;
 });
+//GET all videos uploaded by a user
+Route.get("/users/:user_id/videos", async ({ params }) => {
+  const userId = params.user_id;
+  const videos = await Database.raw(
+    `SELECT * FROM
+      (
+      SELECT
+          all_videos.id AS video_id,
+          all_videos.title,
+          all_videos.description,
+          all_videos.likes,
+          all_videos.user_id,
+          CASE WHEN user_likes.user_likes IS NULL THEN false ELSE true END AS liked,
+          all_videos.url
+      FROM
+          (
+          SELECT
+              videos.id,
+              videos.title,
+              videos.description,
+              videos.url,
+              videos.user_id,
+              CASE WHEN NOT EXISTS (
+                  SELECT * FROM
+                      video_likes
+                  WHERE
+                      videos.id = video_likes.video_id
+              ) THEN 0 ELSE COUNT(videos.id) END AS likes
+          FROM
+              videos
+          LEFT JOIN
+              video_likes
+          ON
+              videos.id = video_likes.video_id
+          GROUP BY
+              videos.id)
+          AS
+              all_videos
+          LEFT JOIN 
+              (
+              SELECT
+                  videos.id,
+                  videos.title,
+                  COUNT(videos.id) AS user_likes
+              FROM
+                  videos
+              JOIN
+                  video_likes
+              ON
+                  videos.id = video_likes.video_id
+              WHERE
+                  video_likes.user_id = 1
+              GROUP BY
+                  videos.id
+              )
+              AS
+                  user_likes
+          ON
+              all_videos.id = user_likes.id
+          ORDER BY
+              all_videos.id
+      ) AS all_vids
+    WHERE
+      all_vids.user_id = ${userId}
+    ;`)
+  return videos.rows
+})
 //GET videos with AGGREGATES total likes and USER likes
 Route.get("/users/:user_id/feed", async ({ params }) => {
   const userId = params.user_id;
