f8d417d9a480a27dfae8088d6f6453f8e8fcd410
Ben Dyer <dyer.ben@protonmail.com>
2019-06-27 10:33:31 +0900

    Update video seeder

--- database/seeds/VideoSeeder.js
+++ database/seeds/VideoSeeder.js
@@ -17,18 +17,27 @@ const Database = use('Database')
 class VideoSeeder {
   async run () {
     const videos = await Database.table('videos').insert([{
-      title: "Uganda Pineapples",
-      description: "We are making delicious pineapple wine!",
-      url: "htts://proud-videos.s3-ap-northeast-1.amazonaws.com/pineapples.mp4",
-      user_id: 4,
-      "created_at": Database.fn.now(),
-      "updated_at": Database.fn.now()
-    },
     {
       url: 'https://proud-videos.s3-ap-northeast-1.amazonaws.com/ug.mp4',
       title: 'My best friend',
       description: 'How I helped my best friend',
       user_id: 1,
+      "created_at": Database.fn.now(),
+      "updated_at": Database.fn.now()
+    },
+    {
+      title: "Big Hero",
+      description: "Promoting young Philippines artists",
+      url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/phil2.mp4",
+      user_id: 3,
+      "created_at": Database.fn.now(),
+      "updated_at": Database.fn.now()
+    },
+    {
+      title: "Georgia",
+      description: "Making Georgia cleaner two bottles at a time",
+      url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/grg.mp4",
+      user_id: 5,
       "created_at": Database.fn.now(),
       "updated_at": Database.fn.now()
     },
@@ -41,21 +50,14 @@ class VideoSeeder {
       "updated_at": Database.fn.now()
     },
     {
-      title: "Tom Waits",
-      description: "Making Georgia cleaner two bottles at a time",
-      url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/grg.mp4",
-      user_id: 5,
+      title: "Uganda Pineapples",
+      description: "We are making delicious pineapple wine!",
+      url: "htts://proud-videos.s3-ap-northeast-1.amazonaws.com/pineapples.mp4",
+      user_id: 1,
       "created_at": Database.fn.now(),
       "updated_at": Database.fn.now()
-    },
-    {
-      title: "Big Hero",
-      description: "Promoting young Philippines artists",
-      url: "https://proud-videos.s3-ap-northeast-1.amazonaws.com/phil2.mp4",
-      user_id: 3,
-      "created_at": Database.fn.now(),
-      "updated_at": Database.fn.now()
-    }])
+    }
+  ])
   }
 }
 
